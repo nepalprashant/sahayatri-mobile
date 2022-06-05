7264dfdc8d7b80469c2750b4ab863e18d7394283
Prashant Nepal <prashantnepal567@gmail.com>
2022-06-05 20:18:19 +0545

    Update location service for Android 12

--- sahayatri/lib/Helper_Classes/location_helper.dart
+++ sahayatri/lib/Helper_Classes/location_helper.dart
@@ -5,18 +5,19 @@ Future<LatLng> accessCurrentLocation() async {
   bool serviceEnabled;
   LocationPermission permission;
 
-  // Test if location services are enabled.
-  serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    return Future.error('Location services are disabled.');
-  }
-
   permission = await Geolocator.checkPermission();
   if (permission == LocationPermission.denied) {
     permission = await Geolocator.requestPermission();
     if (permission == LocationPermission.denied) {
       return Future.error('Location permissions are denied');
     }
+  }
+
+  // Test if location services are enabled.
+  serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    
+    return Future.error('Location services are disabled.');
   }
 
   if (permission == LocationPermission.deniedForever) {
